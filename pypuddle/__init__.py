8e0bf0f239b9bd7ca6b910c8b80c299329a1c711
antonio <antonio.belotti@studenti.unimi.it>
2020-12-22 18:09:49 +0100

    add cd method

---
+++ pypuddle/__init__.py
@@ -0,0 +1,28 @@
+import json
+
+import requests
+
+
+class DataPuddle:
+
+    def __init__(self, api_base_address):
+        self.base_url = api_base_address
+
+        response = requests.get(f"{self.base_url}/sessionkey")
+        if response.status_code == 200:
+            self.key = json.loads(response.content.decode('utf-8')).get("key")
+        else:
+            raise RuntimeError("unable to obtain api key")
+
+    def cd(self, path):
+        url = self.__get_endpoint("cd", path=path)
+        response = requests.get(url)
+        json_response = json.loads(response.content.decode('utf-8'))
+        if json_response.get("outcome") == "error":
+            raise RuntimeError("cd error. Specified path is probably non existent")
+
+    def __get_endpoint(self, action, **kwargs):
+        endpoint = f"{self.base_url}/{action}?key={self.key}"
+        for key, item in kwargs.items():
+            endpoint += f"&{key}={item}"
+        return endpoint
